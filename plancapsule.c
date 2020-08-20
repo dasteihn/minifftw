ae246e77c7c3f93bff1eddf366dc4df57ef36af8
Philipp Stanner <stanner@posteo.de>
2020-08-20 23:45:56 +0200

    all: add license notices

--- plancapsule.c
+++ plancapsule.c
@@ -1,3 +1,21 @@
+/*
+ *  Copyright 2020, Philipp Stanner, <stanner@posteo.de>
+ *
+ * This file is part of Minifftw.
+ * Minifftw is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Minifftw is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Minifftw.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <Python.h>
 #include <stdlib.h>
 #include "util.h"
