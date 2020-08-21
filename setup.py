d447b0a41ba2c5f2d67f7d06dfdf1feebe2a942f
Philipp Stanner <stanner@posteo.de>
2020-08-21 22:48:31 +0200

    setup: fix linking bug

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from distutils.core import setup, Extension
 import os
 
-module1 = Extension('minifftw', sources = ['minifftw.c', 'util.c',
-    'plancapsule.c'])
+module1 = Extension('minifftw',
+        sources = ['minifftw.c', 'util.c', 'plancapsule.c'],
+        libraries = ['fftw3'],
+        )
 
 # Use the mpi-C-Compiler-Wrapper. Setting the environment variable from extern
 # causes awkward behavior.
 # os.environ['CC'] = "mpicc"
 
-setup(name = 'Spami', version = '1.0', description = 'schrott',
-        ext_modules = [module1])
+setup(name = 'minifftw', version = '0.1', description = 'schrott',
+        ext_modules = [module1],
+        author='Philipp Stanner',
+        author_email='stanner@posteo.de')
