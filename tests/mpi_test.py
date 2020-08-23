7ad99c594af16c38c507b0d86fe5a783ee1597d9
Philipp Stanner <stanner@posteo.de>
2020-08-23 21:01:30 +0200

    tests: fix argv

--- tests/mpi_test.py
+++ tests/mpi_test.py
@@ -1,18 +1,19 @@
 import sys
-sys.path.append("../build/lib.linux-x86_64-3.7")
+import time
+sys.path.append("./build/lib.linux-x86_64-3.7")
 
 import minifftw as m
 
-new_argv = []
-for elm in sys.argv:
-    new_argv.append(str(elm))
-    
-
-m.init(["hallo"], 4)
+m.init(sys.argv, 4)
 l = []
 for i in range(0, 8192):
     l.append(-42 + 9001j)
 
 p = m.plan_dft_1d(l, m.FFTW_FORWARD, m.FFTW_ESTIMATE)
 erg = m.execute(p)
+time.sleep(1)
+m.finit()
+time.sleep(2)
 print("Test run successfully! :)")
+time.sleep(2)
+print("und jetzt explodierts...")
