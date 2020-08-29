461d67b425fc35232ca888095c4b953994dd9072
Philipp Stanner <stanner@posteo.de>
2020-08-29 19:37:34 +0200

    tests: adjust mpi to numpy

--- tests/mpi_test.py
+++ tests/mpi_test.py
@@ -5,15 +5,10 @@ sys.path.append("./build/lib.linux-x86_64-3.7")
 import minifftw as m
 
 m.init(sys.argv, 4)
-l = []
-for i in range(0, 8192):
-    l.append(-42 + 9001j)
 
-p = m.plan_dft_1d(l, m.FFTW_FORWARD, m.FFTW_ESTIMATE)
+data = 0 * np.random.random(10) + np.random.random(10) * 1j
+print(data)
+p = m.plan_dft_1d(data, m.FFTW_FORWARD, m.FFTW_ESTIMATE)
 erg = m.execute(p)
-time.sleep(1)
 m.finit()
-time.sleep(2)
 print("Test run successfully! :)")
-time.sleep(2)
-print("und jetzt explodierts...")
