83db96441ef87566caee0797ea0f2aa2f88c730c
Philipp Stanner <stanner@posteo.de>
2020-09-11 18:02:27 +0200

    tests: use two numpy arrays

--- tests/numpy_test.py
+++ tests/numpy_test.py
@@ -6,13 +6,16 @@ sys.path.append("./build/lib.linux-x86_64-3.7")
 
 import minifftw as m
 
+data_len = int(2**20)
+
 m.init(sys.argv, 4)
-data = 0 * np.random.random(10) + np.random.random(10) * 1j
-print(data)
-p = m.plan_dft_1d(data, m.FFTW_FORWARD, m.FFTW_ESTIMATE)
+data_in = np.random.random(data_len) + np.random.random(data_len) * 1j
+data_out = np.zeros(data_len, dtype="complex128")
+#print(data)
+p = m.plan_dft_1d(data_in, data_out, m.FFTW_FORWARD, m.FFTW_ESTIMATE)
 
 erg = m.execute(p)
 print(type(erg))
-print(erg)
+print(len(erg))
 m.finit()
 print("Test run successfully! :)")
